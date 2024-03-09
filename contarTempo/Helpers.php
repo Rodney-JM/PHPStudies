1ade627802a65ba4783db685a89108b0b7249122
Rodney <rondineypatricio@gmail.com>
2024-03-09 12:18:14 -0300

    More studies now, i learn how to do a countTime function and others with PHP language

--- contarTempo/Helpers.php
+++ contarTempo/Helpers.php
@@ -1,6 +1,11 @@
 <?php 
 
-function contarTempo(string $data){
+/**
+ * @param string $data simboliza a data em que o usuario realizou determinada açao
+ * @return string retorna a diferença entre o tempo em que o usuario fez determinada ação pro agora
+ */
+function contarTempo(string $data):string
+{
     $dataAgora = strtotime(date('Y-m-d H:i:s')); //converte a str para segundos
 
     $tempo = strtotime($data);//data que ira ser recebida
@@ -13,4 +18,20 @@ function contarTempo(string $data){
     $semanas = round($diferencaTempo/604800);
     $meses = round($diferencaTempo/2592000);
     $anos = round($diferencaTempo/31104000);
+
+    if($segundos<=60){
+        return 'agora';
+    }elseif($minutos<=60){
+        return $minutos==1 ? 'há um minutos': "há {$minutos} minutos";
+    }elseif($horas<=24){
+        return $horas==1 ? 'há uma hora': "há {$horas} horas";
+    }elseif($dias<=7){
+        return $dias==1 ? 'há um dia': "há {$dias} dias";
+    }elseif($semanas<=4){
+        return $semanas==1? 'há uma semana': "há {$semanas} semanas";
+    }else if($meses<=12){
+        return $meses==1? 'há um mês': "há {$meses} meses";
+    }else{
+        return $anos==1? 'há um ano': "há {$anos} anos";
+    }
 }
--- contarTempo/index.php
+++ contarTempo/index.php
@@ -2,4 +2,4 @@
 
 require 'Helpers.php';
 
-contarTempo('2024-03-08 18:43:2');//Padrao de datas americano
+echo contarTempo('2022-03-08 18:43:2');//Padrao de datas americano
